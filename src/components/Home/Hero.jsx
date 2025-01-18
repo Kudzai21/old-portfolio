b3ff235951e7b9e02e3ff03ee4bd01591aaab14e
Oswell Kudzai Mapuranga <mapurangako@gmail.com>
2025-01-18 21:46:56 +0200

    text edit

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -84,7 +84,7 @@ const Hero = () => {
                             variants={childVariants}
                             className="text-xl md:text-2xl lg:text-3xl mx-5"
                         >
-                            <h1 className="font-semibold text-6xl lg:text-[12rem] tracking-widest pb-5">
+                            <h1 className="font-semibold text-5xl lg:text-[12rem] tracking-widest pb-5">
                                 {["PRODUCT", "DESIGNER"].map((word, wordIndex) => (
                                     <React.Fragment key={wordIndex}>
                                     {word.split("").map((char, index) => (
